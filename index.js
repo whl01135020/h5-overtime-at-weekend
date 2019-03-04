509ab0b6ad23bb997b1a9efd48a3a96a6775cd16
wanghongli <18829289491@163.com>
2019-03-04 20:23:55 +0800

    项目配置fis构建

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ var wrapper = new Vue({
             }, {
                 id: 3,
                 name: '三娃'
-            },{
+            }, {
                 id: 4,
                 name: '四娃'
             }, {
@@ -36,21 +36,21 @@ var wrapper = new Vue({
                 id: 2
             }],
             noAddPerson: [],
-            sixAddPerson:[],
-            sevenAddPerson:[],
+            sixAddPerson: [],
+            sevenAddPerson: [],
             currentSelect: null,
-            oldvalue:'',
-            newvalue:'',
-            oldName:'',
-            newName:'',
-            sixAddValue:0,
-            sevenAddValue:0,
-            noAddValue:0,
+            oldvalue: '',
+            newvalue: '',
+            oldName: '',
+            newName: '',
+            sixAddValue: 0,
+            sevenAddValue: 0,
+            noAddValue: 0,
 
         };
     },
     computed: {
-        noAddDisable(){
+        noAddDisable() {
             return this.currentSelect == 1;
         }
     },
@@ -59,7 +59,7 @@ var wrapper = new Vue({
             echarts.dispose($('#chart')[0]);
             let chartInstance = echarts.init($('#chart')[0]);
             let option = {
-                color : ["#38B5FA", "#FFB750", "#99DC3D"],
+                color: ["#38B5FA", "#FFB750", "#99DC3D"],
                 title: {
                     left: 'center',
                     text: '统计饼图',
@@ -74,8 +74,8 @@ var wrapper = new Vue({
                 },
                 legend: {
                     left: 'center',
-                    data: ['No','Saturday', 'Sunday'],
-                    bottom:0
+                    data: ['No', 'Saturday', 'Sunday'],
+                    bottom: 0
                 },
                 grid: {
                     x: '100px',
@@ -115,12 +115,12 @@ var wrapper = new Vue({
             };
             chartInstance.setOption(option);
         },
-        loadBarChart(){
+        loadBarChart() {
             echarts.dispose($('#barChart')[0]);
             let chartInstance = echarts.init($('#barChart')[0]);
             let option = {
-                color:"#5ea9fc",
-                title : {
+                color: "#5ea9fc",
+                title: {
                     left: 'center',
                     text: '统计柱状图',
                     textStyle: {
@@ -128,7 +128,7 @@ var wrapper = new Vue({
                         fontSize: 16,
                     },
                 },
-                xAxis : {
+                xAxis: {
                     type: 'category',
                     boundaryGap: true,
                     axisTick: {
@@ -138,150 +138,149 @@ var wrapper = new Vue({
                         lineStyle: {
                             color: '#dddddd'
                         }
-                },
-                splitLine: {
-                    show: false,
-                    lineStyle: {
-                        color: ['#ddd']
                     },
-                    width: 1
-                },
-                axisLabel: {
-                    margin: 15,
-                    textStyle: {
-                        color: '#333333',
-                        fontSize: 12
-                    }
-                },
-                data: ['No','Saturday','Sunday']
-            },
-            tooltip : {
-                trigger: 'axis',
-                axisPointer: {
-                    lineStyle: {
-                        color: '#2e7bcc',
-                        width: 80,
-                        opacity: 0.2
+                    splitLine: {
+                        show: false,
+                        lineStyle: {
+                            color: ['#ddd']
+                        },
+                        width: 1
                     },
+                    axisLabel: {
+                        margin: 15,
+                        textStyle: {
+                            color: '#333333',
+                            fontSize: 12
+                        }
+                    },
+                    data: ['No', 'Saturday', 'Sunday']
                 },
-                extraCssText: 'box-shadow: 0 0 4px rgba(0, 0, 0, 0.12);',
-                textStyle: {
-                    color: '#333',
-                    fontWeight: 'bold'
-                },
-                padding: 16,
-                backgroundColor: 'rgba(255, 255, 255, .95)',
-                borderRadius: 4,
-                borderWidth: 1,
-                borderColor: '#bbbbbb',
-                shadowColor: '#000000',
-                shadowBlur: 4,
-            },
-            //这里是y坐标
-            yAxis : {
-                // boundaryGap: true,
-                type: 'value',
-                scale: true,
-                max: null,
-                axisLine: {
-                    show: false
-                },
-                axisLabel: {
-                    show: true,
-                    interval: 'auto',
-                    formatter: '{value}',
-                    margin: 5,
+                tooltip: {
+                    trigger: 'axis',
+                    axisPointer: {
+                        lineStyle: {
+                            color: '#2e7bcc',
+                            width: 80,
+                            opacity: 0.2
+                        },
+                    },
+                    extraCssText: 'box-shadow: 0 0 4px rgba(0, 0, 0, 0.12);',
                     textStyle: {
-                        color: "#333333",
-                        fontSize: 12
-                    }
+                        color: '#333',
+                        fontWeight: 'bold'
+                    },
+                    padding: 16,
+                    backgroundColor: 'rgba(255, 255, 255, .95)',
+                    borderRadius: 4,
+                    borderWidth: 1,
+                    borderColor: '#bbbbbb',
+                    shadowColor: '#000000',
+                    shadowBlur: 4,
                 },
-                splitNumber:2,
-                axisTick: {
-                    show: false
+                //这里是y坐标
+                yAxis: {
+                    // boundaryGap: true,
+                    type: 'value',
+                    scale: true,
+                    max: null,
+                    axisLine: {
+                        show: false
+                    },
+                    axisLabel: {
+                        show: true,
+                        interval: 'auto',
+                        formatter: '{value}',
+                        margin: 5,
+                        textStyle: {
+                            color: "#333333",
+                            fontSize: 12
+                        }
+                    },
+                    splitNumber: 2,
+                    axisTick: {
+                        show: false
+                    },
+                    show: true
                 },
-                show: true
-            },
-           
-            legend : {
-                orient: 'horizontal',
-                x: 'center',
-                y: 'bottom',
-                top: 382,
-                itemGap: 40,
-                data: ['统计结果柱状图']
-            },
-            grid : {
-                left: '0%',
-                right: '5%',
-                bottom: '10%',
-                top: 45,
-                containLabel: true
-            },
-            series : {
-                name: '统计结果柱状图',
-                barWidth: '15px',
-                type: 'bar',
-                data:[this.noAddValue,this.sixAddValue,this.sevenAddValue]
-            },
+
+                legend: {
+                    orient: 'horizontal',
+                    x: 'center',
+                    y: 'bottom',
+                    top: 382,
+                    itemGap: 40,
+                    data: ['统计结果柱状图']
+                },
+                grid: {
+                    left: '0%',
+                    right: '5%',
+                    bottom: '10%',
+                    top: 45,
+                    containLabel: true
+                },
+                series: {
+                    name: '统计结果柱状图',
+                    barWidth: '15px',
+                    type: 'bar',
+                    data: [this.noAddValue, this.sixAddValue, this.sevenAddValue]
+                },
 
             }
             chartInstance.setOption(option);
         },
         changeSelect(item) {
-            if(item==1 && this.personData.name){
+            if (item == 1 && this.personData.name) {
                 this.currentSelect = 1;
-                if(item ==1 ){
+                if (item == 1) {
                     this.noAddPerson.push(this.personData.name);
                     let newArr = this.noAddPerson;
                     this.noAddPerson = [...new Set(newArr)];
                     this.getChartData();
                 }
-            }
-            else if(item==2){
+            } else if (item == 2) {
                 this.currentSelect = 2;
             }
         },
-        getSixAdd(){
+        getSixAdd() {
             let vm = this;
-            $(function(){
-                $("#sixSelect").change(function(){
-                   if(vm.personData.name && vm.currentSelect == 2){
-                    vm.sixAddPerson.push(vm.personData.name + '周六' + vm.personData.six);
-                    let newArr =  vm.sixAddPerson;
-                    vm.sixAddPerson = [...new Set(newArr)];
-                    vm.getChartData();
-                   }
+            $(function () {
+                $("#sixSelect").change(function () {
+                    if (vm.personData.name && vm.currentSelect == 2) {
+                        vm.sixAddPerson.push(vm.personData.name + '周六' + vm.personData.six);
+                        let newArr = vm.sixAddPerson;
+                        vm.sixAddPerson = [...new Set(newArr)];
+                        vm.getChartData();
+                    }
                 });
-            });    
+            });
         },
-        getSevenAdd(){
+        getSevenAdd() {
             let vm = this;
-            $(function(){
-                $("#sevenSelect").change(function(){
-                   if(vm.personData.name && vm.currentSelect == 2){
-                    vm.sevenAddPerson.push(vm.personData.name + '周天' + vm.personData.seven);
-                    let newArr =  vm.sevenAddPerson;
-                    vm.sevenAddPerson = [...new Set(newArr)];
-                    vm.getChartData();
-                   }
+            $(function () {
+                $("#sevenSelect").change(function () {
+                    if (vm.personData.name && vm.currentSelect == 2) {
+                        vm.sevenAddPerson.push(vm.personData.name + '周天' + vm.personData.seven);
+                        let newArr = vm.sevenAddPerson;
+                        vm.sevenAddPerson = [...new Set(newArr)];
+                        vm.getChartData();
+                    }
                 });
-            });    
+            });
         },
-        onmousedownFunction(){
+        onmousedownFunction() {
             let vm = this;
-            $(function(){
-                $("#nameSelect").mousedown(function(item){
+            $(function () {
+                $("#nameSelect").mousedown(function (item) {
                     vm.oldName = $(this).children('option:selected').val();
                 })
             })
         },
-        onchangeFunction(){
+        onchangeFunction() {
             let vm = this;
-            $(function(){
-                $("#nameSelect").change(function(item){
+            $(function () {
+                $("#nameSelect").change(function (item) {
                     vm.newName = $(this).children('option:selected').val();
-                    if(vm.newName !== vm.oldName){
+                    if (vm.newName !== vm.oldName) {
                         vm.currentSelect = null;
                         vm.personData.six = '';
                         vm.personData.seven = '';
@@ -289,19 +288,19 @@ var wrapper = new Vue({
                 })
             })
         },
-        removeNoAdd(noAdd,index){
-            this.noAddPerson.splice(index, 1); 
+        removeNoAdd(noAdd, index) {
+            this.noAddPerson.splice(index, 1);
             this.getChartData();
         },
-        removeSixAdd(noAdd,index){
-            this.sixAddPerson.splice(index,1);
+        removeSixAdd(noAdd, index) {
+            this.sixAddPerson.splice(index, 1);
             this.getChartData();
         },
-        removeSevenAdd(noAdd,index){
-            this.sevenAddPerson.splice(index,1);
+        removeSevenAdd(noAdd, index) {
+            this.sevenAddPerson.splice(index, 1);
             this.getChartData();
         },
-        getChartData(){
+        getChartData() {
             this.sixAddValue = this.sixAddPerson.length;
             this.sevenAddValue = this.sevenAddPerson.length;
             this.noAddValue = this.noAddPerson.length;
@@ -316,7 +315,7 @@ var wrapper = new Vue({
         this.onmousedownFunction();
         this.onchangeFunction();
         this.getChartData();
-      
+
 
     }
 })
